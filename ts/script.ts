082917868e6d6d3ec83bc3be67925f6633fe9136
reinaldo henrique <naldo-mar@hotmail.com>
2022-12-30 11:57:18 -0300

    update styles

--- ts/script.ts
+++ ts/script.ts
@@ -5,36 +5,16 @@ let clockUpdate: NodeJS.Timer | null;
 
 clockUpdate = setInterval(() => updateClock(), 1000);
 
-const moveClock = ({ offsetX, offsetY }: MouseEvent) => {
-  if (clock) {
-    const { clientHeight, clientWidth } = clock;
-    const rotateX = (offsetX - clientWidth / 2) * 0.05;
-    const rotateY = (offsetY - clientHeight / 2) * 0.1;
-
-    clock.style.transform = `perspective(1000px) rotateX(${-rotateY}deg) rotateY(${rotateX}deg)`;
-  }
-};
-
-const outClock = () => {
-  if (clock) {
-    clock.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
-    if (!clockUpdate) pauseClock();
-  }
-};
-
 const pauseClock = () => {
   if (clockUpdate) {
     clearInterval(clockUpdate);
     clockUpdate = null;
     clock?.classList.add('paused');
-    clock?.addEventListener('mousemove', moveClock);
   } else {
     updateClock();
     clockUpdate = setInterval(() => updateClock(), 1000);
     clock?.classList.remove('paused');
-    clock?.removeEventListener('mousemove', moveClock);
   }
 };
 
 clock?.addEventListener('click', pauseClock);
-clock?.addEventListener('mouseleave', outClock);
